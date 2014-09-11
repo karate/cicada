1ce4efbf4df6d3b02a5f626a1322371dc5ce01a4
karate <tsimpidisd@gmail.com>
2014-09-11 23:18:26 +0300

    Fixes #2 When editind a transaction, account balance is updated

--- Controller/ActionsController.php
+++ Controller/ActionsController.php
@@ -10,30 +10,7 @@ class ActionsController extends AppController {
 
 	public function add() {
 		if ($this->request->is('post')) {
-            $this->Action->create();
-            if ($this->Action->save($this->request->data)) {
-            	$acc_id = $this->request->data['Action']['account'];
-
-            	$this->loadModel('Account');
-            	$this->Account->id = $acc_id;
-            	$account = $this->Account->find('first', array('conditions' => array('id' => $acc_id)));
-            	$balance = $account['Account']['balance'];
-
-            	$type = $this->request->data['Action']['type'];
-
-            	if ($type == '1') {
-            		$balance -= $this->request->data['Action']['ammount'];
-            	}
-            	elseif ($type == '2') {
-            		$balance += $this->request->data['Action']['ammount'];	
-            	}
-
-            	if ($this->Account->saveField('balance', $balance)) {
-	                $this->Session->setFlash(__('Transaction has been saved. New balance is '. $balance));
-	                return $this->redirect(array('action' => 'index'));
-            	}
-            }
-            $this->Session->setFlash(__('Unable to save transaction.'));
+			$this->_create_transaction($this->request->data);
         }
 
         $this->loadModel('Account');
@@ -43,32 +20,16 @@ class ActionsController extends AppController {
 	    $this->set('action_type', $this->ActionType->find('list', array('fields' => array('id', 'description'))));
 	}
 
-	public function delete($id, $account, $ammount) {
+	public function delete($id = NULL) {
+		if (!$id) {
+			throw new NotFoundException(__('Invalid transaction'));
+	    }
+
 		if ($this->request->is('get')) {
 	        throw new MethodNotAllowedException();
 	    }
 
-    	// If transaction was deleted successfully, update account balance
-	    if ($this->Action->delete($id)) {
-
-	    	$this->loadModel('Account');
-	    	$account = $this->Account->find('first', array('conditions' => array('Account.id' => $account)));
-
-	    	$balance = $account['Account']['balance'];
-
-	    	// This works even when deleting withdraws ($ammount is negative)
-	    	$updated_balance = $balance - $ammount;
-	    	
-	    	// Update account with new balance
-	    	$this->Account->id = $account;
-	    	$this->Account->saveField('balance', $updated_balance);
-
-	    	$account_name = $account['Account']['description'];
-	        $this->Session->setFlash(
-	            __("Transaction has been deleted. New balance of $account_name is: $updated_balance")
-	        );
-
-	    }
+	    $this->_delete_transaction($id);
 	    return $this->redirect(array('action' => 'index'));
 	}
 
@@ -78,17 +39,18 @@ class ActionsController extends AppController {
 	    }
 
 	    $action = $this->Action->findById($id);
+
 	    if (!$action) {
 	        throw new NotFoundException(__('Invalid transaction'));
 	    }
 
 	    if ($this->request->is(array('action', 'put'))) {
-	        $this->Action->id = $id;
-	        if ($this->Action->save($this->request->data)) {
-	            $this->Session->setFlash(__('Transaction has been updated.'));
-	            return $this->redirect(array('action' => 'index'));
-	        }
-	        $this->Session->setFlash(__('Unable to update transaction.'));
+	    	// delete transaction
+	    	$this->_delete_transaction($id);
+
+	    	// add new transaction
+	    	$this->_create_transaction($this->request->data);
+	    	
 	    }
 
 	    if (!$this->request->data) {
@@ -140,6 +102,71 @@ class ActionsController extends AppController {
 		
 
 		return $actions;
+	}
+
+
+	/* * * * * * * * * * * * *
+	 *   PRIVATE FUNCTIONS   *
+	 * * * * * * * * * * * * */
+	private function _delete_transaction($transaction_id) {
+		// Load transaction details
+	    $action = $this->Action->find('first', array('conditions' => array('Action.id' => $transaction_id)));
+	    $ammount = $action['Action']['ammount'];
+	    $account_id = $action['Action']['account'];
+	    
+	    if ($action['Action']['type'] == 1) {
+			$ammount *= -1;
+		}
+
+    	// If transaction was deleted successfully, update account balance
+	    if ($this->Action->delete($transaction_id)) {
+
+	    	// Load account details
+	    	$this->loadModel('Account');
+	    	$account = $this->Account->find('first', array('conditions' => array('Account.id' => $account_id)));
+
+	    	$balance = $account['Account']['balance'];
+
+	    	// This works even when deleting withdraws ($ammount is negative)
+	    	$updated_balance = $balance - $ammount;
+	    	
+	    	// Update account with new balance
+	    	$this->Account->id = $account;
+	    	$this->Account->saveField('balance', $updated_balance);
+
+	    	$account_name = $account['Account']['description'];
+	        $this->Session->setFlash(
+	            __("Transaction has been deleted. New balance of $account_name is: $updated_balance")
+	        );
+	    }
+	}
+
+	private function _create_transaction($details) {
+		$this->Action->create();
+		if ($this->Action->save($details)) {
+			$account_id = $details['Action']['account'];
+
+			$this->loadModel('Account');
+			$account = $this->Account->find('first', array('conditions' => array('id' => $account_id)));
+			$balance = $account['Account']['balance'];
+
+			$type = $this->request->data['Action']['type'];
+
+			if ($type == '1') {
+				$balance -= $details['Action']['ammount'];
+			}
+			elseif ($type == '2') {
+				$balance += $details['Action']['ammount'];	
+			}
+
+			$this->Account->id = $account_id;
+			if ($this->Account->saveField('balance', $balance)) {
+		        $this->Session->setFlash(__('Transaction has been saved. New balance is '. $balance));
+		        return $this->redirect(array('action' => 'index'));
+			}
+		}
+
+		$this->Session->setFlash(__('Unable to save transaction.'));
 	}
 }
 
